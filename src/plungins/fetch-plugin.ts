550a1be1f958fb70e0d4b11b0fa40bfbb4d1184f
andy tsai <andy5566w@gmail.com>
2021-04-04 10:18:06 +0800

    refactor: wasm改從unpkg下載

--- src/plungins/fetch-plugin.ts
+++ src/plungins/fetch-plugin.ts
@@ -8,43 +8,50 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log(
-          '-------------------------------------------------------------'
-        )
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          }
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
         }
+      })
 
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         // check to see if we have already fetched this file and if it is in the cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         )
-
         if (cachedResult) return cachedResult
+      })
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path)
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx'
 
         const cssString = data
           .replace(/\n/g, '')
           .replace(/'/g, "\\'")
           .replace(/"/g, '\\"')
 
-        const contents =
-          fileType === 'css'
-            ? `
+        const contents = `
               const style = document.createElement('style');
               style.innerText = '${cssString}';
               document.head.appendChild(style);
               `
-            : data
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        }
+        await fileCache.setItem(args.path, result)
+
+        return result
+      })
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path)
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         }
         await fileCache.setItem(args.path, result)
