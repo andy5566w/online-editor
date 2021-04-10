c87b19a92cdcff7ceaefd8d6cea525fe793aacef
andy tsai <andy5566w@gmail.com>
2021-04-10 13:13:13 +0800

    feat: 新增store

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CodeCell from './components/CodeCell'
+import { Provider } from 'react-redux'
+import { store } from './state'
 
 const App = () => {
   return (
-    <div>
-      <CodeCell />
-    </div>
+    <Provider store={store}>
+      <div>
+        <CodeCell />
+      </div>
+    </Provider>
   )
 }
 
