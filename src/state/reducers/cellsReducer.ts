da343244c4168ac94f78d3b45713aae190ed775f
andy tsai <andy5566w@gmail.com>
2021-04-11 14:21:38 +0800

    feat: 新增immer去操作action-creator

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,6 +1,7 @@
 import { ActionType } from '../action-types'
 import { CellActions } from '../actions'
 import { Cell } from '../cell'
+import produce from 'immer'
 
 interface CellsState {
   loading: boolean
@@ -18,31 +19,47 @@ const initialState = {
   data: {},
 }
 
-const reducer = (
-  state: CellsState = initialState,
-  action: CellActions
-): CellsState => {
-  switch (action.type) {
-    case ActionType.MOVE_CELL:
-      return state
-    case ActionType.DELETE_CELL:
-      return state
-    case ActionType.INSERT_CELL_BEFORE:
-      return state
-    case ActionType.UPDATE_CELL:
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [action.payload.id]: {
-            ...state.data[action.payload.id],
-            content: action.payload.content,
-          },
-        },
-      }
-    default:
-      return state
+const reducer = produce(
+  (state: CellsState = initialState, action: CellActions) => {
+    switch (action.type) {
+      case ActionType.MOVE_CELL:
+        const { direction } = action.payload
+        const index = state.order.findIndex((id) => id === action.payload.id)
+        const target_index = direction === 'up' ? index - 1 : index + 1
+        if (target_index < 0 || target_index > state.order.length - 1) return
+
+        state.order[index] = state.order[target_index]
+        state.order[target_index] = action.payload.id
+        return state
+      case ActionType.DELETE_CELL:
+        delete state.data[action.payload.id]
+        state.order = state.order.filter((id) => id !== action.payload.id)
+        return state
+      case ActionType.INSERT_CELL_BEFORE:
+        const cell: Cell = {
+          content: '',
+          type: action.payload.type,
+          id: randomId(),
+        }
+        state.data[cell.id] = cell
+
+        const foundIndex = state.order.findIndex(
+          (id) => id === action.payload.id
+        )
+        if (foundIndex === -1) state.order.push(cell.id)
+        else state.order.splice(foundIndex, 0)
+
+        return state
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload
+        state.data[id].content = content
+        return state
+      default:
+        return state
+    }
   }
-}
+)
+
+const randomId = () => Math.random().toString(16).slice(2)
 
 export default reducer
